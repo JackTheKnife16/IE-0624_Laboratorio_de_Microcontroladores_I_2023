02880291695119889d238fa204bc2226c39d1bf4
JackTheKnife16 <jacktheknife16@gmail.com>
2023-03-31 00:02:23 -0600

    Modificacion del .c para seguir trabajando con funciones llamadas desde el main, se sacaron las variables para que fueran globales

--- Laboratorio01/src/tombola.c
+++ Laboratorio01/src/tombola.c
@@ -1,5 +1,18 @@
 #include <pic14/pic12f675.h>
- 
+
+//char vector_resultados[16];
+char head = 0; // apunta al ultimo resultado
+char ball_counter = 0;
+
+    
+short int time = 1;
+char flag_eje = 0;
+char counter = 0;
+char num = 0b11111111; 
+char bandera = 0;
+char random = 0;
+
+
 //To compile:
 //sdcc -mpic14 -p16f675 blink.c
  
@@ -20,33 +33,30 @@
  
 //To program the chip using pk2cmd:
 //pk2cmd -M -PPIC16f887 -Fblink.hex
- 
+
+// estados
+#define IDLE 0
+#define FIRST 1
+#define NORMAL 2
+#define LAST 3
+#define BLINK 4
+
+void calcular_random(char* random);
 void delay (unsigned int tiempo);
 void generador_pulso(unsigned int tiempo);
 void reset_counter(unsigned int tiempo);
 void setear_bit2bit(char* flag_eje, char* num, char* counter, unsigned int time);
+char obtener_codigo(char* random);
+
+char current_state = IDLE;
 
 
- 
-void main(void)
-{
+
+void main(void) {
 
     TRISIO = 0b00001000; //Poner todos los pines como salidas
 	GPIO = 0x00; //Poner pines en bajo
-
-    char vector_resultados[16];
-    char head = 0; // apunta al ultimo resultado
-    char ball_counter = 0;
-
     
-    short int time = 1;
-    char flag_eje = 0;
-    char counter = 0;
-    char num = 0b11111111; 
-    char dig1 = 0;
-    char dig2 = 0;
-    char bandera = 0;
-    char random = 0;
 
 
     // inicializar los valores del vector en 100
@@ -54,54 +64,61 @@ void main(void)
     //Loop forever
     while ( 1 ) {
 
-        if(random == 99){
-            random = 0;
-        }
-        else {
-            random++;
-        }
-
-        
-        /// la encargada de cambiar los valores
+        calcular_random(&random);
         if(bandera == 0 && GP3 == 1){
             bandera = 1;
             flag_eje = 0;
-            dig1 = random % 10;
-            dig2 = random / 10;
-            num = dig2 << 4;
-            num |= dig1;
-            //ball_counter;
-            
+            num = obtener_codigo(&random);
         }
-
         if(GP3 == 0){
             bandera = 0;
         }
-
-
-        if(flag_eje == 0){
-            setear_bit2bit(&flag_eje, &num, &counter, time);
-        }
+        
+        setear_bit2bit(&flag_eje, &num, &counter, time);
+        
     }
  
+}
+
+char obtener_codigo(char* random){
+    char num;
+    char dig1 = (*random) % 10;
+    char dig2 = (*random) / 10;
+    num = dig2 << 4;
+    num |= dig1;
+    return num;
+}
+
+
+// esta funcion es un simple contador que aumenta su valor cada vez que
+// es llamada, va de 0 a 99.
+void calcular_random(char* random){
+    if(*random == 99){
+        *random = 0;
+    }
+    else {
+        (*random)++;
+    }
 }
 
 // esta funcion es la encargada de la comunicacion serial con el circuito externo
 // maneja los pines GP0 que comunica el dato, el pin GP1 que genera el pulso
 // y el pin GP2 que resetea el counter externo como medida de seguridad.
 void setear_bit2bit(char* flag_eje, char* num, char* counter, unsigned int time){
-    GP0 = *num & 1;
-    *num = *num >> 1;
+    if(*flag_eje == 0){
+        GP0 = *num & 1;
+        *num = *num >> 1;
     
-    if(*counter == 0){
-        reset_counter(time);
-    }
-    generador_pulso(time);
+        if(*counter == 0){
+            reset_counter(time);
+        }
+        generador_pulso(time);
         
-    (*counter)++;
-    if(*counter == 8){
-        *counter = 0;
-        *flag_eje = 1;
+        (*counter)++;
+        if(*counter == 8){
+            *counter = 0;
+            *flag_eje = 1;
+        }
     }
 }
 
