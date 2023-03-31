e6d85f3b12b04d6644d3bc5ad1116f27a4ff46db
JackTheKnife16 <jacktheknife16@gmail.com>
2023-03-30 22:44:56 -0600

    Se cambio de microcontrolador a un P12F683  porque el original daba muchos problemas, por lo que se han cambiado todos los archivos originales, el archivo C se ha ido mejorando para implementar el algoritmo completo del laboratorio.

--- Laboratorio01/src/tombola.c
+++ Laboratorio01/src/tombola.c
@@ -24,61 +24,99 @@
 void delay (unsigned int tiempo);
 void generador_pulso(unsigned int tiempo);
 void reset_counter(unsigned int tiempo);
-void reset_prueba(unsigned int* tiempo);
+void setear_bit2bit(char* flag_eje, char* num, char* counter, unsigned int time);
 
-/* void enviar_datos(unsigned int* flag_, */
-/*                   unsigned int* num_, */
-/*                   unsigned int* counter_, */
-/*                   unsigned int time_); */
+
  
 void main(void)
 {
 
-    TRISIO = 0b00000000; //Poner todos los pines como salidas
+    TRISIO = 0b00001000; //Poner todos los pines como salidas
 	GPIO = 0x00; //Poner pines en bajo
- 
-    unsigned int time = 500;
-    unsigned int flag_eje = 0;
-    unsigned int counter = 0;
-    unsigned int num = 0b10000111;
+
+    char vector_resultados[16];
+    char head = 0; // apunta al ultimo resultado
+    char ball_counter = 0;
+
+    
+    short int time = 1;
+    char flag_eje = 0;
+    char counter = 0;
+    char num = 0b11111111; 
+    char dig1 = 0;
+    char dig2 = 0;
+    char bandera = 0;
+    char random = 0;
+
+
+    // inicializar los valores del vector en 100
  
     //Loop forever
     while ( 1 ) {
+
+        if(random == 99){
+            random = 0;
+        }
+        else {
+            random++;
+        }
+
         
-        //enviar_datos(&flag_eje, &num, &counter, time);
+        /// la encargada de cambiar los valores
+        if(bandera == 0 && GP3 == 1){
+            bandera = 1;
+            flag_eje = 0;
+            dig1 = random % 10;
+            dig2 = random / 10;
+            num = dig2 << 4;
+            num |= dig1;
+            //ball_counter;
+            
+        }
+
+        if(GP3 == 0){
+            bandera = 0;
+        }
+
 
         if(flag_eje == 0){
-            GP0 = num & 1;
-            num = num >> 1;
-            reset_prueba(&time);
-
-            if(counter == 0){
-                reset_counter(time);
-            }
-            generador_pulso(time);
-        
-            counter++;
-            if(counter == 8){
-                counter = 0;
-                flag_eje = 1;
-            }
+            setear_bit2bit(&flag_eje, &num, &counter, time);
         }
     }
  
 }
 
+// esta funcion es la encargada de la comunicacion serial con el circuito externo
+// maneja los pines GP0 que comunica el dato, el pin GP1 que genera el pulso
+// y el pin GP2 que resetea el counter externo como medida de seguridad.
+void setear_bit2bit(char* flag_eje, char* num, char* counter, unsigned int time){
+    GP0 = *num & 1;
+    *num = *num >> 1;
+    
+    if(*counter == 0){
+        reset_counter(time);
+    }
+    generador_pulso(time);
+        
+    (*counter)++;
+    if(*counter == 8){
+        *counter = 0;
+        *flag_eje = 1;
+    }
+}
 
-
+// funcion que agrega un delay de tiempo segun lo que se desee, es a puro ojo
 void delay(unsigned int tiempo)
 {
 	unsigned int i;
 	unsigned int j;
 
 	for(i=0;i<tiempo;i++)
-	  for(j=0;j<500;j++);
+	  for(j=0;j<200;j++);
 }
 
-
+// esta funcion genera los pulsos para el contador que maneja el clk de los flip flops de datos
+// y el clk del contador que decide hacia que flip flop se dirige el dato
 void generador_pulso(unsigned int tiempo){
     delay(tiempo);
     GP1 = 1;
@@ -86,6 +124,8 @@ void generador_pulso(unsigned int tiempo){
     GP1 = 0;
 }
 
+// esta funcion es una medida de seguridad para resetear el contador siempre que se terminan de
+// enviar los datos seriales
 void reset_counter(unsigned int tiempo){
     delay(tiempo);
     GP2 = 1;
@@ -93,33 +133,3 @@ void reset_counter(unsigned int tiempo){
     GP2 = 0;
 }
 
-void reset_prueba(unsigned int* tiempo){
-    unsigned int tie = *tiempo;
-    delay(tie);
-    GP5 = 1;
-    delay(tie);
-    GP5 = 0;
-    
-}
-
-/* void enviar_datos(unsigned int* flag_, */
-/*                   unsigned int* num_, */
-/*                   unsigned int* counter_, */
-/*                   unsigned int time_){ */
-    
-/*     if(*flag_ == 0){ */
-/*         GP0 = *num_ & 1; */
-/*         *num_ = *num_ >> 1; */
-
-/*         if(*counter_ == 0){ */
-/*             reset_counter(time_); */
-/*         } */
-/*         generador_pulso(time_); */
-        
-/*         *counter_++; */
-/*         if(*counter_ == 8){ */
-/*             *counter_ = 0; */
-/*             *flag_ = 1; */
-/*         } */
-/*     } */
-/* } */
